e36de11771f4aa49d0d440c9b88e3c3827e75d10
otonbraga <otonpc@gmail.com>
2016-03-12 13:41:46 -0300

    arquivos dos novos modulos

---
+++ rmi_example/src/InterfaceServidorMat.java
@@ -0,0 +1,10 @@
+
+import java.rmi.*;
+
+public interface InterfaceServidorMat extends Remote
+{
+    public double soma(double a, double b) throws RemoteException;
+    public double subtrai(double a, double b) throws RemoteException;
+    public double multiplica(double a, double b) throws RemoteException;
+    public double divide(double a, double b) throws RemoteException;
+} 
